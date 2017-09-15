7acf1091c347ec01eb9f811a6b55a8a11bfa335c
刘东明 <itucas@foxmail.com>
2017-09-15 17:26:14 +0800

    first commit

--- javascripts/listView.js
+++ javascripts/listView.js
@@ -1,35 +1,4 @@
 $(function(){
-
-   $("#className").kendoComboBox({
-      placeholder: "请选择班级...",
-      dataTextField: "name",
-      dataValueField: "name",
-      filter: "contains",
-      autoClose: false,
-      dataSource:{
-        transport: {
-          read: {
-            url: "json/className.json",
-            dataType:"json"
-          }
-        }
-      }//dataSource
-   });
-   $("#student").kendoComboBox({
-      placeholder: "请选择学生...",
-      dataTextField: "StudentName",
-      dataValueField: "id",
-      filter: "contains",
-      autoClose: false,
-      dataSource:{
-        transport: {
-          read: {
-            url: "json/student.json",
-            dataType:"json"
-          }
-        }
-      }//dataSource
-   });
    $("#teacher").kendoComboBox({
       placeholder: "请选择教师...",
       dataTextField: "teacherName",
@@ -43,38 +12,112 @@ $(function(){
             dataType:"json"
           }
         }
-      }//dataSource
+      },//dataSource
+      change: function(e){
+         var value=this.value();
+         if(value=="") return;
+         //console.log("teacher",value);
+         var className=$("#className").data("kendoComboBox");
+             className.dataSource.filter({field: "teacher", value: value, operator: "eq"});
+         var student=$("#student").data("kendoComboBox");
+             student.dataSource.filter({field: "TeacherName", value: value, operator: "eq"});
+         filterListView();
+      }
    });
+   $("#className").kendoComboBox({
+      placeholder: "请选择班级...",
+      dataTextField: "name",
+      dataValueField: "name",
+      filter: "contains",
+      autoClose: false,
+      dataSource:{
+        transport: {
+          read: {
+            url: "json/className.json",
+            dataType:"json"
+          }
+        }
+      },//dataSource
+      change: function(e){
+         var value=this.value();
+         if(value=="") return;
+         //console.log("className",value);
+         var student=$("#student").data("kendoComboBox");
+             student.dataSource.filter({field: "className", value: value, operator: "eq"});
+         filterListView();
+      }
+   });
+   $("#student").kendoComboBox({
+      placeholder: "请选择学生...",
+      dataTextField: "StudentName",
+      dataValueField: "StudentName",
+      filter: "contains",
+      autoClose: false,
+      dataSource:{
+        transport: {
+          read: {
+            url: "json/student.json",
+            dataType:"json"
+          }
+        }
+      },//dataSource
+      change: function(e){
+         var value=this.value();
+         if(value=="") return;
+         //console.log("student",value);
+         var student=$("#student").data("kendoComboBox");
+             student.dataSource.filter({field: "className", value: value, operator: "eq"});
+         filterListView();
+      }
+   });
+   function filterListView(){
+      var teacher=$("#teacher").data("kendoComboBox");
+      var className=$("#className").data("kendoComboBox");
+      var student=$("#student").data("kendoComboBox");
+      var field=[];
+      if(teacher.value()){
+         field.push({field: "TeacherName", value: teacher.value(), operator: "eq"})
+      }
+      if(className.value()){
+         field.push({field: "className", value: className.value(), operator: "eq"})
+      }
+      if(student.value()){
+         field.push({field: "StudentName", value: student.value(), operator: "eq"})
+      }
+
+      var listView=$("#listView").data("kendoListView");
+          listView.dataSource.filter(field);
+   }
 });
 
 $(document).ready(function () {
      var dataSource = new kendo.data.DataSource({
-            transport: {
-                read:{
-                    url: "json/student.json",
-                    dataType: "json"
-                },
-                parameterMap: function(options, operation) {
-                    if (operation !== "read" && options.models) {
-                        return {models: kendo.stringify(options.models)};
-                    }
-                }
+        transport: {
+            read:{
+                url: "json/student.json",
+                dataType: "json"
             },
-            batch: true,
-            pageSize: 4,
-            schema: {
-                model: {
-                    id: "ProductID",
-                    fields: {
-                        ProductID: { editable: false, nullable: true },
-                        StudentName: "StudentName",
-                        Degree: "Degree",
-                        className: "className",
-                        TeacherName: "TeacherName"
-                    }
+            parameterMap: function(options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {models: kendo.stringify(options.models)};
                 }
             }
-        });
+        },
+        batch: true,
+        pageSize: 4,
+        schema: {
+            model: {
+                id: "ProductID",
+                fields: {
+                    ProductID: { editable: false, nullable: true },
+                    StudentName: "StudentName",
+                    Degree: "Degree",
+                    className: "className",
+                    TeacherName: "TeacherName"
+                }
+            }
+        }
+    });
 
 
     $("#pager").kendoPager({
